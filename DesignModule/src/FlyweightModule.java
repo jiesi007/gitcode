48ada3afb8fb30abdc996f4e10b78a4a0396310f
xuesongyuan <xuesongyuan@meitaun.com>
2019-03-26 10:45:51 +0800

    jiesi

--- DesignModule/src/FlyweightModule.java
+++ DesignModule/src/FlyweightModule.java
@@ -1,11 +1,44 @@
+import java.util.Hashtable;
+
 /**
  * Created by jiesi on 2019/1/3.
  */
 public class FlyweightModule {
+    public static void main(String[] args) {
+        WebsiteFactory f = new WebsiteFactory();
+        Website fx = f.GetwetsiteCategory("产品展示");
+        fx.use();
+        Website fy = f.GetwetsiteCategory("产品展示");
+        fy.use();
+        Website fz = f.GetwetsiteCategory("产品展示");
 
+    }
 }
 
 abstract class Website {
     public abstract void use();
+}
+
+class ConcreteWebsite {
+    public void use() {
+        System.out.println();
+    }
+}
+
+class WebsiteFactory {
+    private Hashtable flyweight = new Hashtable();
+
+    public Website GetwetsiteCategory(String key) {
+        if (!flyweight.containsKey(key)) {
+            flyweight.put(key, new ConcreteWebsite());
+        }
+        return ((Website) flyweight.keySet());
+    }
+
+    public int getwebsiteCount() {
+        return flyweight.size();
+    }
 
 }
+
+
